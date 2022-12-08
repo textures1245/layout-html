985a9cf4316459c02a7b15ddf951cf9644c724c1
Traiphakh stihhikaew <sirprak1245@gmail.com>
2022-12-08 17:09:53 +0700

    second init (final version)

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res) {
+    res.render('index');
+  });
+
+app.listen(8000);
+console.log('Server is listening on port 6000');
+
